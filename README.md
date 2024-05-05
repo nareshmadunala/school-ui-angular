4eeedbc007a0121e8224c630b9dbecd284e9a29d
Naresh Madunala <nareshmadunala@users.noreply.github.com>
2024-05-04 18:09:38 -0700

    signup form added

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
-# SchoolUi
+# SchoolUiAngular
 
+<<<<<<< Updated upstream
 Navigate to `http://localhost:4200/`. The application will automatically reload if you change any of the source files.
+=======
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 17.3.6.
+>>>>>>> Stashed changes
 
---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { LoginComponent } from './components/login/login.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SignupComponent,
+    DashboardComponent,
+    LoginComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    FormsModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
